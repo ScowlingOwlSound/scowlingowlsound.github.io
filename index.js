8024375f78f828d0eb435b25121b272c25438dc9
ScowlingOwlSound <david@scowlingowlsound.com>
2020-02-26 22:52:58 +1100

    dialog

--- index.js
+++ index.js
@@ -1,17 +1,65 @@
-// See https://github.com/dialogflow/dialogflow-fulfillment-nodejs
-// for Dialogflow fulfillment library docs, samples, and to report issues
+/**
+ * Copyright 2017 Google Inc. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 'use strict';
 
 const functions = require('firebase-functions');
-const {WebhookClient} = require('dialogflow-fulfillment');
-const {Card, Suggestion} = require('dialogflow-fulfillment');
+const { WebhookClient } = require('dialogflow-fulfillment');
+const { Card, Suggestion } = require('dialogflow-fulfillment');
+const { Carousel } = require('actions-on-google');
 
 process.env.DEBUG = 'dialogflow:debug'; // enables lib debugging statements
+
+// URLs for images used in card rich responses
+const imageUrl = 'https://developers.google.com/actions/images/badges/XPM_BADGING_GoogleAssistant_VER.png';
+const imageUrl2 = 'https://lh3.googleusercontent.com/Nu3a6F80WfixUqf_ec_vgXy_c0-0r4VLJRXjVFF_X_CIilEu8B9fT35qyTEj_PEsKw';
+const linkUrl = 'https://assistant.google.com/';
 
 exports.dialogflowFirebaseFulfillment = functions.https.onRequest((request, response) => {
   const agent = new WebhookClient({ request, response });
   console.log('Dialogflow Request headers: ' + JSON.stringify(request.headers));
   console.log('Dialogflow Request body: ' + JSON.stringify(request.body));
+
+  function googleAssistantOther(agent) {
+    let conv = agent.conv(); // Get Actions on Google library conversation object
+    conv.ask('Please choose an item:'); // Use Actions on Google library to add responses
+    conv.ask(new Carousel({
+      title: 'Google Assistant',
+      items: {
+        'WorksWithGoogleAssistantItemKey': {
+          title: 'Works With the Google Assistant',
+          description: 'If you see this logo, you know it will work with the Google Assistant.',
+          image: {
+            url: imageUrl,
+            accessibilityText: 'Works With the Google Assistant logo',
+          },
+        },
+        'GoogleHomeItemKey': {
+          title: 'Google Home',
+          description: 'Google Home is a powerful speaker and voice Assistant.',
+          image: {
+            url: imageUrl2,
+            accessibilityText: 'Google Home'
+          },
+        },
+      },
+    }));
+    // Add Actions on Google library responses to your agent's response
+    agent.add(conv);
+  }
 
   function welcome(agent) {
     agent.add(`Welcome to my agent!`);
@@ -22,45 +70,32 @@ exports.dialogflowFirebaseFulfillment = functions.https.onRequest((request, resp
     agent.add(`I'm sorry, can you try again?`);
   }
 
-  function forward(agent) {
-    agent.add('You are now walking forward');
-     }
-  // // Uncomment and edit to make your own intent handler
-  // // uncomment `intentMap.set('your intent name here', yourFunctionHandler);`
-  // // below to get this function to be run when a Dialogflow intent is matched
-  // function yourFunctionHandler(agent) {
-  //   agent.add(`This message is from Dialogflow's Cloud Functions for Firebase editor!`);
-  //   agent.add(new Card({
-  //       title: `Title: this is a card title`,
-  //       imageUrl: 'https://developers.google.com/actions/images/badges/XPM_BADGING_GoogleAssistant_VER.png',
-  //       text: `This is the body text of a card.  You can even use line\n  breaks and emoji! 💁`,
-  //       buttonText: 'This is a button',
-  //       buttonUrl: 'https://assistant.google.com/'
-  //     })
-  //   );
-  //   agent.add(new Suggestion(`Quick Reply`));
-  //   agent.add(new Suggestion(`Suggestion`));
-  //   agent.setContext({ name: 'weather', lifespan: 2, parameters: { city: 'Rome' }});
-  // }
+  function other(agent) {
+    agent.add(`This message is from Dialogflow's Cloud Functions for Firebase editor!`);
+    agent.add(new Card({
+        title: `Title: this is a card title`,
+        imageUrl: imageUrl,
+        text: `This is the body text of a card.  You can even use line\n  breaks and emoji! 💁`,
+        buttonText: 'This is a button',
+        buttonUrl: linkUrl
+      })
+    );
+    agent.add(new Suggestion(`Quick Reply`));
+    agent.add(new Suggestion(`Suggestion`));
+    agent.setContext({ name: 'weather', lifespan: 2, parameters: { city: 'Rome' }});
+  }
 
-  // // Uncomment and edit to make your own Google Assistant intent handler
-  // // uncomment `intentMap.set('your intent name here', googleAssistantHandler);`
-  // // below to get this function to be run when a Dialogflow intent is matched
-  // function googleAssistantHandler(agent) {
-  //   let conv = agent.conv(); // Get Actions on Google library conv instance
-  //   conv.ask('Hello from the Actions on Google client library!') // Use Actions on Google library
-  //   agent.add(conv); // Add Actions on Google library responses to your agent's response
-  // }
-  // // See https://github.com/dialogflow/fulfillment-actions-library-nodejs
-  // // for a complete Dialogflow fulfillment library Actions on Google client library v2 integration sample
-
-  // Run the proper function handler based on the matched Dialogflow intent name
+  // Run the proper handler based on the matched Dialogflow intent
   let intentMap = new Map();
   intentMap.set('Default Welcome Intent', welcome);
   intentMap.set('Default Fallback Intent', fallback);
-
-  intentMap.set('forward',forward);
-  // intentMap.set('your intent name here', yourFunctionHandler);
-  // intentMap.set('your intent name here', googleAssistantHandler);
+  // if requests for intents other than the default welcome and default fallback
+  // is from the Google Assistant use the `googleAssistantOther` function
+  // otherwise use the `other` function
+  if (agent.requestSource === agent.ACTIONS_ON_GOOGLE) {
+    intentMap.set(null, googleAssistantOther);
+  } else {
+    intentMap.set(null, other);
+  }
   agent.handleRequest(intentMap);
 });
